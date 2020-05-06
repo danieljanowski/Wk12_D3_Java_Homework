3d8467007d8c2fa4f25f08ea505a0bca3fb83ec2
danieljanowski <daniel.janowski@gmail.com>
2020-05-06 22:59:29 +0100

    MVP 9/9 done, Extensions 1/4

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,20 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    ArrayList<Attraction> attractions;
+    ArrayList<Stall> stalls;
+
+    public ThemePark() {
+        this.attractions = new ArrayList<>();
+        this.stalls = new ArrayList<>();
+    }
+
+//    public ArrayList<IReviewed> getAllReviewed(){
+//
+//    }
 }
