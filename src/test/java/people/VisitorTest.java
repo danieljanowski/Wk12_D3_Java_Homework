968dd3bbd387d299afd599c6f1b4fff5c4c0a5dd
danieljanowski <daniel.janowski@gmail.com>
2020-05-07 07:58:13 +0100

    MVP 9/9 done, Extensions 3/4

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,7 @@
 package people;
 
+import attractions.Dodgems;
+import attractions.Playground;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,11 +9,15 @@ import static org.junit.Assert.assertEquals;
 
 public class VisitorTest {
 
+    Playground playground;
+    Dodgems dodgems;
     Visitor visitor;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        playground = new Playground("Slides",10);
+        dodgems = new Dodgems("Dodge-ems",6);
     }
 
     @Test
@@ -27,5 +33,12 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void canAddVisitedAttraction(){
+        visitor.addVisited(playground);
+        visitor.addVisited(dodgems);
+        assertEquals(2,visitor.countVisitedAttractions());
     }
 }
